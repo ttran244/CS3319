9ce56bacbc01b1b2e8b50ec2b191707f3e373a27
Tam Tran <ttran244@uwo.ca>
2014-11-21 21:38:46 +0000

    connect db to page

--- asn3/index.php
+++ asn3/index.php
@@ -1,11 +1,25 @@
+/*
+ * index.php
+ * 
+ * Computer Science 3319a
+ * Assignment 3
+ *
+ * Author: Tommy Tran (ttran244@uwo.ca)
+ *
+ * The php web page. Creates a web page that allows the grad secretary
+ * to make changes to the TA database
+ */
+
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="utf-8">
-<title> </title>
+<title>TA Database</title>
 </head>
 <body>
-
-
+<?php
+include "connectdb.php";
+?>
+<h1>Welcome to the TA database</h1>
 </body>
 </html>
